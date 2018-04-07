55dc8085daebf7d612ed6818f29833a06e005586
gloss-water <gloss.water@gmail.com>
2018-04-07 03:25:50 -0600

    unauthed discord session display matches discord styling

--- src/components/Authorization/DiscordLogin.js
+++ src/components/Authorization/DiscordLogin.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { apiHost } from '../../utils';
 
 const DiscordLogin = () => (
-    <div>
-        <a href={`${apiHost}/api/discord/login`}>Login with Discord</a>
+    <div className='discord-session-w'>
+        <div className='discord-session'>
+            <div className='discord-display-wrapper'>
+                <div className='discord-display'>
+                    <a className='discord-login-link' href={`${apiHost}/api/discord/login`}>Login with Discord</a>
+                    <br />
+                </div>
+            </div>
+        </div>
     </div>
 );
 
